5ebf30f74d8473020826b48a1436e3df88ce3f33
Giovani Wahl <giovaniwahl@gmail.com>
2020-04-09 09:25:39 -0300

    2 alteração

--- binary tree/binary_tree.cpp
+++ binary tree/binary_tree.cpp
@@ -3,12 +3,11 @@
 # include <string.h>
 # include <locale.h>
 
-#define lenght 30 // define o tamnaho da string
+#define lenght 30 
 
-// declação de funções
-int menu(); // menu principal
+int menu(); 
 
-struct  student_register  // modelo da estrutura do registro de alunos
+struct  student_register  
 {
 	int RU;
 	char name_student[lenght];
@@ -17,7 +16,7 @@ struct  student_register  // modelo da estrutura do registro de alunos
 
 typedef struct student_register Student;
 
-struct node // modelo do nó da arvore
+struct node 
 {
 	Student Ru;
 	node* right;
@@ -26,175 +25,143 @@ struct node // modelo do nó da arvore
 
 typedef node Node;
 
-Node* init() // fução para  inicializar a arvore
+Node* init() 
 {
 	return NULL;
 }
 
-Student RuCreate(int ru, char nome[], char email[]) // função para retornar o item x de Student para a função insert
+Student RuCreate(int ru, char nome[], char email[]) 
 {
 	Student student;
-	student.RU = ru; // o codigo RU de student_register vai receber o parametro  informado
+	student.RU = ru; 
 	strcpy_s(student.name_student, nome);
 	strcpy_s(student.email_student, email);
 	return student;
 }
 
-Node* insert(Node* root, Student newRu) // função para inserir dados na arvore
+Node* insert(Node* root, Student newRu) 
 {
-	if (root == NULL)// insere dados na raiz (root) vazia
+	if (root == NULL)
 	{
-		Node* new_node = NULL; // cria um novo nó 
-		new_node = (Node*)malloc(sizeof(Node)); // aloca a memória para o novo nó
-		new_node->Ru = newRu; // novo nó recebe o item RU do Student vindo de fora (x)
-		new_node->right = NULL; // por ser a ultima posição do nó (folha) ponteiro para proximo item a Direita recebe nulo
-		new_node->left = NULL; // por ser a ultima posição do nó (folha) ponteiro para proximo item a Esquerda recebe nulo
+		Node* new_node = NULL;  
+		new_node = (Node*)malloc(sizeof(Node)); 
+		new_node->Ru = newRu; 
+		new_node->right = NULL; 
+		new_node->left = NULL; 
 		return new_node;
 	}
-	else // caso a arvore ja tenha algum registro (nó)
+	else 
 	{
-		if (newRu.RU > root->Ru.RU) // se o novo item for maior que o nó, alocar a Direita do mesmo
-		{
+		if (newRu.RU > root->Ru.RU) 
 			root->right = insert(root->right, newRu);
-		}
-		else if (newRu.RU < root->Ru.RU) // se o novo item for menor que o nó, alocar a Esquerda deste
-		{
+		else if (newRu.RU < root->Ru.RU) 
 			root->left = insert(root->left, newRu);
-		}
-
 	}
 	return root;
 }
 
-// imprimir toda a arvore  ordenada  Menor ->> Maior
 void printSorted(Node* root)
 {
-	if (root != NULL) // caso a raiz (root) esteja vazio (null) não sera executada a função
+	if (root != NULL) 
 	{
-		printSorted(root->left); // vai chamar ela mesma para imprimir o que houver a Esquerda do nó anterior
-		printf("\n %d \n", root->Ru); // vai imprimir o RU do nó
-		printSorted(root->right); // vai chamar ela mesma para imprimir o que houver a Direita do nó anterior
+		printSorted(root->left); 
+		printf("\n %d \n", root->Ru); 
+		printSorted(root->right);
 	}
 }
 
-// libera os nós
 void treeFree(Node* root)
 {
 	if (root != NULL)
 	{
-		treeFree(root->left); // vai liberar o que houver a Esquerda do nó anterior
-		treeFree(root->right); // vai liberar o que houver a Direita do nó anterior
-		free(root); // vai liberar a raiz da arvore
+		treeFree(root->left);
+		treeFree(root->right);
+		free(root);
 	}
 }
 
-// localiza um RU 
 Node* search(Node* root, int searchRu)
 {
 	if (root != NULL)
 	{
 		if (root->Ru.RU == searchRu)
-		{
 			return root;
-		}
 		else
 		{
 			if (root->Ru.RU < searchRu)
-			{
 				return search(root->right, searchRu);
-			}
 			else
-			{
 				return search(root->left, searchRu);
-			}
 		}
 	}
 	return NULL;
 }
 
-// inicializa uma consulta
-
 int main() {
-	setlocale(LC_ALL, "Portuguese"); // define a localização e padrão de escrita
-	// while ((c = getchar()) != '\n' && c != EOF) {}; // limpar buffer teclado
+	setlocale(LC_ALL, "Portuguese"); 
 	char giovani[lenght];
 	char giovaniemail[lenght];
 	strcpy_s(giovani, "Giovani");
 	strcpy_s(giovaniemail, "giovaniwahl@gmail.com");
-
 	char antonio[lenght];
 	char antonioemail[lenght];
 	strcpy_s(antonio, "Antonio");
 	strcpy_s(antonioemail, "antonio@gmail.com");
-
 	char maira[lenght];
 	char mairaemail[lenght];
 	strcpy_s(maira, "Maira");
 	strcpy_s(mairaemail, "maira@gmail.com");
-
 	char sueli[lenght];
 	char sueliemail[lenght];
 	strcpy_s(sueli, "Sueli");
 	strcpy_s(sueliemail, "sueli@gmail.com");
-
 	char jose[lenght];
 	char joseemail[lenght];
 	strcpy_s(jose, "Jose");
 	strcpy_s(joseemail, "jose@gmail.com");
-
 	char albino[lenght];
 	char albinoemail[lenght];
 	strcpy_s(albino, "Albino");
 	strcpy_s(albinoemail, "albino@gmail.com");
-
 	char jean[lenght];
 	char jeanemail[lenght];
 	strcpy_s(jean, "Jean");
 	strcpy_s(jeanemail, "jean@gmail.com");
-
 	char henrique[lenght];
 	char henriqueemail[lenght];
 	strcpy_s(henrique, "Henrique");
 	strcpy_s(henriqueemail, "henrique@gmail.com");
-
 	char cristina[lenght];
 	char cristinaemail[lenght];
 	strcpy_s(cristina, "Cristina");
 	strcpy_s(cristinaemail, "cristina@gmail.com");
-
 	char maria[lenght];
 	char mariaemail[lenght];
 	strcpy_s(maria, "Maria");
 	strcpy_s(mariaemail, "maria@gmail.com");
-
 	char daniele[lenght];
 	char danieleemail[lenght];
 	strcpy_s(daniele, "Daniele");
 	strcpy_s(danieleemail, "daniele@gmail.com");
-
 	char casiano[lenght];
 	char casianoemail[lenght];
 	strcpy_s(casiano, "Casiano");
 	strcpy_s(casianoemail, "casiano@gmail.com");
-
 	char miguel[lenght];
 	char miguelemail[lenght];
 	strcpy_s(miguel, "Miguel");
 	strcpy_s(miguelemail, "miguel@gmail.com");
-
 	char fabio[lenght];
 	char fabioemail[lenght];
 	strcpy_s(fabio, "Fabio");
 	strcpy_s(fabioemail, "fabio@gmail.com");
-
 	char osama[lenght];
 	char osamaemail[lenght];
 	strcpy_s(osama, "Osama");
 	strcpy_s(osamaemail, "osama@gmail.com");
 
-
 	Node* root;
-	root = (Node*)malloc(sizeof(Node)); // cria um novo nó e aloca a memória
+	root = (Node*)malloc(sizeof(Node));
 	root = NULL;
 	Node* tmp;
 
@@ -205,16 +172,14 @@ int main() {
 
 		switch (op)
 		{
-		case 0: // Sair -- encerrar o programa
-			treeFree(root); // libera a memória
+		case 0:
+			treeFree(root);
 			exit(0);
 			break;
-
-		case 1: // insersão dos dados -- ler o arquivo txt
-			system("cls"); // limpa a tela
-
-			root = init(); // cria a arvore, inicializa o 1 nó
-			root = insert(root, RuCreate(2787739, giovani, giovaniemail)); // insere os dados no nó
+		case 1:
+			system("cls");
+			root = init();
+			root = insert(root, RuCreate(2787739, giovani, giovaniemail));
 			root = insert(root, RuCreate(5874813, antonio, antonio));
 			root = insert(root, RuCreate(8750143, maira, mairaemail));
 			root = insert(root, RuCreate(7515656, sueli, sueliemail));
@@ -229,68 +194,49 @@ int main() {
 			root = insert(root, RuCreate(6601487, miguel, miguelemail));
 			root = insert(root, RuCreate(3768301, fabio, fabioemail));
 			root = insert(root, RuCreate(5330385, osama, osamaemail));
-
 			printf("\n ARVORE CRIADA COM SECESSO \n");
-
 			system("pause");
 			break;
-
-		case 2: // localiza uma RU especifica e imprime seus dados na tela
-			system("cls"); // limpa a tela
+		case 2:
+			system("cls");
 			int RU;
-
 			if (root == NULL)
-			{
 				printf("\n ARVORE NÃO GERADA \n");
-			}
 
 			if (root != NULL)
 			{
 				printf("\n DIGITE A RU DESEJADA \n");
 				scanf_s("%d", &RU);
-				while ((c = getchar()) != '\n' && c != EOF) {}; // limpar buffer teclado
-
+				while ((c = getchar()) != '\n' && c != EOF) {};
 				tmp = search(root, RU);
 				if (tmp == NULL)
-				{
 					printf("\n  REGISTRO NÃO LOCALIZADO \n");
-				}
 				else
-				{
 					printf("\n RU: %d \n Nome: %s \n E-mail: %s \n", tmp->Ru.RU, tmp->Ru.name_student, tmp->Ru.email_student);
-				}
 			}
 			system("pause");
 			break;
-
-		case 3: // imprime toda a arvore
-			system("cls"); // limpa a tela
-
+		case 3:
+			system("cls");
 			if (root != NULL)
 			{
 				printf("\n ARVORE ORDENADA \n");
-				printSorted(root);// imprimir toda a arvore  ordenada  Menor ->> Maior
+				printSorted(root);
 			}
 			if (root == NULL)
-			{
 				printf("\n ARVORE NÃO GERADA \n");
-			}
 			system("pause");
 			break;
-
-		default: // chama o menu de novo
+		default:
+			return 0;
 			break;
 		}
-
 	}
-
-	treeFree(root); // libera a memória
-
+	treeFree(root);
 	system("pause");
 	return 0;
 }
 
-// Menu
 int menu()
 {
 	system("cls");
@@ -302,7 +248,6 @@ int menu()
 	printf("\n // *** 3 IMPRIMIR TODA A ARVORE \n");
 	printf("\n // 0 SAIR \n");
 	scanf_s("%d", &op);
-	while ((c = getchar()) != '\n' && c != EOF) {}; // limpar buffer teclado
-
+	while ((c = getchar()) != '\n' && c != EOF) {};
 	return op;
 }
