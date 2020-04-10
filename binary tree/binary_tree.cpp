6d078b846741b8353eaf305ac901a61aa492141c
Giovani Wahl <giovaniwahl@gmail.com>
2020-04-10 15:05:17 -0300

    tree bst

--- binary tree/binary_tree.cpp
+++ binary tree/binary_tree.cpp
@@ -99,80 +99,16 @@ Node* search(Node* root, int searchRu)
 
 int main() {
 	setlocale(LC_ALL, "Portuguese"); 
-	int RU = NULL;
-	char nome[lenght];
-	char email[lenght];
-
-	char giovani[lenght];
-	char giovaniemail[lenght];
-	strcpy_s(giovani, "Giovani");
-	strcpy_s(giovaniemail, "giovaniwahl@gmail.com");
-	char antonio[lenght];
-	char antonioemail[lenght];
-	strcpy_s(antonio, "Antonio");
-	strcpy_s(antonioemail, "antonio@gmail.com");
-	char maira[lenght];
-	char mairaemail[lenght];
-	strcpy_s(maira, "Maira");
-	strcpy_s(mairaemail, "maira@gmail.com");
-	char sueli[lenght];
-	char sueliemail[lenght];
-	strcpy_s(sueli, "Sueli");
-	strcpy_s(sueliemail, "sueli@gmail.com");
-	char jose[lenght];
-	char joseemail[lenght];
-	strcpy_s(jose, "Jose");
-	strcpy_s(joseemail, "jose@gmail.com");
-	char albino[lenght];
-	char albinoemail[lenght];
-	strcpy_s(albino, "Albino");
-	strcpy_s(albinoemail, "albino@gmail.com");
-	char jean[lenght];
-	char jeanemail[lenght];
-	strcpy_s(jean, "Jean");
-	strcpy_s(jeanemail, "jean@gmail.com");
-	char henrique[lenght];
-	char henriqueemail[lenght];
-	strcpy_s(henrique, "Henrique");
-	strcpy_s(henriqueemail, "henrique@gmail.com");
-	char cristina[lenght];
-	char cristinaemail[lenght];
-	strcpy_s(cristina, "Cristina");
-	strcpy_s(cristinaemail, "cristina@gmail.com");
-	char maria[lenght];
-	char mariaemail[lenght];
-	strcpy_s(maria, "Maria");
-	strcpy_s(mariaemail, "maria@gmail.com");
-	char daniele[lenght];
-	char danieleemail[lenght];
-	strcpy_s(daniele, "Daniele");
-	strcpy_s(danieleemail, "daniele@gmail.com");
-	char casiano[lenght];
-	char casianoemail[lenght];
-	strcpy_s(casiano, "Casiano");
-	strcpy_s(casianoemail, "casiano@gmail.com");
-	char miguel[lenght];
-	char miguelemail[lenght];
-	strcpy_s(miguel, "Miguel");
-	strcpy_s(miguelemail, "miguel@gmail.com");
-	char fabio[lenght];
-	char fabioemail[lenght];
-	strcpy_s(fabio, "Fabio");
-	strcpy_s(fabioemail, "fabio@gmail.com");
-	char osama[lenght];
-	char osamaemail[lenght];
-	strcpy_s(osama, "Osama");
-	strcpy_s(osamaemail, "osama@gmail.com");
-
 	Node* root;
 	root = (Node*)malloc(sizeof(Node));
-	root = NULL;
+	root = init();
 	Node* tmp;
 
 	while (1)
 	{
 		int op = menu();
 		int c;
+		int RU = NULL;
 
 		switch (op)
 		{
@@ -180,35 +116,31 @@ int main() {
 			treeFree(root);
 			exit(0);
 			break;
-
 		case 1:
-			root = insert(root, RuCreate(RU, nome, email));
+			char nome[lenght];
+			char email[lenght];
+			system("cls");
+			printf("\n INFORME O RU DO ESTUDANTE: \n");
+			scanf_s("%d", &RU);
+			while ((c = getchar()) != '\n' && c != EOF) {};
+			printf("\n DIGITE NOME \n");
+			gets_s(nome);
+			printf("\n DIGITE O E-MAIL \n");
+			gets_s(email);
+			root = insert(root, RuCreate(RU, nome,email));
 			break;
-
 		case 2:
 			system("cls");
-			root = init();
+			char giovani[lenght];
+			char giovaniemail[lenght];
+			strcpy_s(giovani, "Giovani");
+			strcpy_s(giovaniemail, "giovaniwahl@gmail.com");
 			root = insert(root, RuCreate(2787739, giovani, giovaniemail));
-			root = insert(root, RuCreate(5874813, antonio, antonio));
-			root = insert(root, RuCreate(8750143, maira, mairaemail));
-			root = insert(root, RuCreate(7515656, sueli, sueliemail));
-			root = insert(root, RuCreate(2162033, jose, joseemail));
-			root = insert(root, RuCreate(5736213, albino, albinoemail));
-			root = insert(root, RuCreate(8481484, jean, jeanemail));
-			root = insert(root, RuCreate(2155664, henrique, henriqueemail));
-			root = insert(root, RuCreate(5724550, cristina, cristinaemail));
-			root = insert(root, RuCreate(4181602, maria, mariaemail));
-			root = insert(root, RuCreate(7862577, daniele, danieleemail));
-			root = insert(root, RuCreate(6334235, casiano, casianoemail));
-			root = insert(root, RuCreate(6601487, miguel, miguelemail));
-			root = insert(root, RuCreate(3768301, fabio, fabioemail));
-			root = insert(root, RuCreate(5330385, osama, osamaemail));
 			printf("\n ARVORE CRIADA COM SECESSO \n");
 			system("pause");
 			break;
 		case 3:
 			system("cls");
-			int RU;
 			if (root == NULL)
 				printf("\n ARVORE NÃO GERADA \n");
 
